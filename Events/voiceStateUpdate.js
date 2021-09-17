81e3b67ab95c65533de7a8fe7ca8959b721843f0
Shinyepo <shinyepo42@gmail.com>
2021-09-17 12:27:57 +0200

    Small fix for VoiceStateUpdate param

--- Events/voiceStateUpdate.js
+++ Events/voiceStateUpdate.js
@@ -1,6 +1,11 @@
 module.exports = {
     name: 'voiceStateUpdate',
     on: true,
+    /**
+    * @param {Client} client Current Discord client
+    * @param {GuildMember} oldState Guild member's voice state before change.
+    * @param {GuildMember} newState Guild member's voice state after change.
+    */
     async execute(client, oldState, newState) {
         // console.log(oldState);
         // console.log(newState);
