b24a5de4202abd066d67fc965450da65f1934974
Shinyepo <shinyepo42@gmail.com>
2021-09-17 12:14:47 +0200

    .

--- Commands/esp.js
+++ Commands/esp.js
@@ -7,8 +7,8 @@ module.exports = {
         description: 'Gowno do robienia gowna :)',
     },
     /**
-     * @param {Client} client tak kurwa
-     * @param {APIMessage} message tak nie kurwa
+     * @param {Client} client Current Discord client
+     * @param {APIMessage} message Message from Command Handler
      */
     async execute(client, message) {
         client.emit(message.content, message.author, message.author);
