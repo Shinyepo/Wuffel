72b1db991f68c811bbc21168897856bcd1249e55
Shinyepo <shinyepo42@gmail.com>
2022-11-06 12:28:23 +0100

    traffic tracker memberCount updater

--- src/Services/TrafficService.ts
+++ src/Services/TrafficService.ts
@@ -1,6 +1,7 @@
 import { EntityManager } from "@mikro-orm/knex";
 import { GuildMember } from "discord.js";
 import { GuildTraffic } from "../Entities/GuildTraffic";
+import { Settings } from "../Entities/Settings";
 
 export const addTraffic = async (
   em: EntityManager,
@@ -15,7 +16,13 @@ export const addTraffic = async (
     nickname: nickname ?? undefined,
     joined,
   });
-
   await context.persistAndFlush(newEntry);
+  const settings = await context
+    .fork()
+    .findOne(Settings, { guildId: guild.id });
+  if (!settings) return;
+  settings.userCount = guild.memberCount.toString();
+  await context.persistAndFlush(settings);
+
   return;
 };
