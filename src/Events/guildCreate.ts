37ee84a47270cefd0707d3b97e6ae18c630beaf3
Shinyepo <shinyepo42@gmail.com>
2022-11-06 12:27:21 +0100

    guild settings revamp

--- src/Events/guildCreate.ts
+++ src/Events/guildCreate.ts
@@ -1,11 +1,13 @@
 import { Guild, TextBasedChannel } from "discord.js";
 import { EventType, WuffelClient } from "../../types";
+import { activateSettings } from "../Services/SettingsService";
 import { consoleTimestamp } from "../Utilities/timestamp";
 
 export = {
   name: "guildCreate",
   on: true,
   async execute(client: WuffelClient, _: TextBasedChannel, guild: Guild) {
+    await activateSettings(client.em.fork(), guild);
     console.log(
       consoleTimestamp() +
         " " +
--- src/Events/guildDelete.ts
+++ src/Events/guildDelete.ts
@@ -1,11 +1,13 @@
 import { Guild, TextBasedChannel } from "discord.js";
 import { EventType, WuffelClient } from "../../types";
+import { deactivateSettings } from "../Services/SettingsService";
 import { consoleTimestamp } from "../Utilities/timestamp";
 
 export = {
   name: "guildDelete",
   on: true,
   async execute(client: WuffelClient, _: TextBasedChannel, guild: Guild) {
+    await deactivateSettings(client.em.fork(), guild);
     console.log(
       consoleTimestamp() +
         " " +
